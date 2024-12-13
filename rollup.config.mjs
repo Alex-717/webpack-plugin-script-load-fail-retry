07c3b9b017fa9f49936fac7f9021d1a97ef36f6f
Alex-717 <liangyuhao717@163.com>
2024-12-13 18:02:19 +0800

    提交代码

---
+++ rollup.config.mjs
@@ -0,0 +1,29 @@
+import terser from '@rollup/plugin-terser'
+import copy from 'rollup-plugin-copy'
+
+export default [
+  {
+    input: 'lib/scriptRetryLogic.js',
+    output: {
+      file: 'dist/scriptRetryLogic.js',
+      format: 'iife', // 输出格式：立即执行函数表达式
+    },
+    plugins: [terser()],
+  },
+  {
+    input: 'lib/scriptRetryApendChild.js',
+    output: {
+      file: 'dist/scriptRetryApendChild.js',
+      format: 'iife',
+    },
+    plugins: [
+      terser(),
+      copy({
+        targets: [
+          { src: 'lib/ScriptRetryPlugin.js', dest: 'dist' }
+        ]
+      })
+    ], // 使用 Terser 插件进行压缩
+  }
+];
+
